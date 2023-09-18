3073f0a5d4ceff81f7c18fc43eec0e96cf15636d
ehddnr301 <dy950328@gmail.com>
2023-09-18 13:08:47 +0000

    Find Users Sentence
    유저 Seq값을 받아서 sentence를 조회하는 로직입니다.

--- src/v1/sentence/port/in/sentence.service.interface.ts
+++ src/v1/sentence/port/in/sentence.service.interface.ts
@@ -2,5 +2,6 @@ import { Sentence } from '../../entities/sentence.entity';
 import { CreateSentenceDto } from '../../dto/create-sentence.dto';
 
 export interface ISentenceService {
+  findByUserSeq(userSeq: number): Promise<Sentence[]>;
   create(createSentenceDto: CreateSentenceDto): Promise<Sentence>;
 }
