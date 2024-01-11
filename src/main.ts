5188af43074f39d2c66cbc31b921c00fbbac4caa
Allen / 임기원 <l4279625@gmail.com>
2024-01-11 22:51:16 +0900

    nginx로 리버스 프록시 구현 및 ssl 적용 (#60)

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as fs from 'fs';
-import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
 
 async function bootstrap() {
-  let httpsOptions: HttpsOptions | undefined;
-  if (process.env.SSL === 'true') {
-    httpsOptions = {
-      key: fs.readFileSync(`${process.cwd()}/dist/privkey.pem`),
-      cert: fs.readFileSync(`${process.cwd()}/dist/cert.pem`),
-    };
-  }
-
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
-    httpsOptions,
   });
   app.enableCors();
   app.useGlobalPipes(
