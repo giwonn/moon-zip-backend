e91c55a7902e39cc1f1f4a3d31a948f97e4c163e
Allen / 임기원 <l4279625@gmail.com>
2024-01-14 00:26:21 +0900

    dev.sh 개선 (#69)

--- dev.sh
+++ dev.sh
@@ -1,10 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-# Docker Compose 시작
-docker compose -f docker-compose.dev.yml up --build --force-recreate -d
+docker_stop() {
+  docker compose -f docker-compose.dev.yml down -v  --rmi local --remove-orphans --timeout 0
+}
 
-# 로그를 실시간으로 보여줌
-docker compose -f docker-compose.dev.yml logs -f
+docker_start() {
+  docker compose -f docker-compose.dev.yml up --build --force-recreate
+}
 
-# 로그를 종료하면 Docker Compose 종료 및 삭제
-docker compose -f docker-compose.dev.yml down -v  --rmi local --remove-orphans --timeout 0
+clear() {
+  # 컨테이너 생성중에 종료시 에러 발생하여 딜레이를 줌
+  sleep 1.5
+  docker_stop
+}
+
+# SIGINT : Ctrl + C 와 같이 강제로 종료되면 실행
+# EXIT : 스크립트가 종료되면 실행
+trap clear EXIT
+
+# 실행중인 컴포즈가 있다면 종료 및 삭제
+docker_stop
+
+# 도커 컴포즈 실행
+docker_start
