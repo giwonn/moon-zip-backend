52200eebf2eaead2f50b3288079dcaa778cf86e3
giwonn <l4279625@gmail.com>
2024-01-14 22:41:21 +0900

    개발 환경에서 액세스 토큰 미사용시 header에 user_id 추가 강제하도록 변경

--- src/common/decorator/user-id.decorator.ts
+++ src/common/decorator/user-id.decorator.ts
@@ -1,5 +1,4 @@
 import {
-  BadRequestException,
   createParamDecorator,
   InternalServerErrorException,
 } from '@nestjs/common';
@@ -11,16 +10,6 @@ export const UserId = createParamDecorator((_, ctx) => {
     throw new InternalServerErrorException(
       'UserId 데코레이터를 사용하려면 AccessTokenGuard 또는 RefreshTokenGuard가 선행되어야 합니다.',
     );
-  }
-
-  if (process.env.NODE_ENV === 'development') {
-    if (!request.query?.userId) {
-      throw new BadRequestException(
-        'dev환경에서는 Query Parameter로 userId를 넣어주어야 합니다.',
-      );
-    }
-
-    return request.query?.userId;
   }
 
   return request.userId;
--- src/common/guard/global/access-token.guard.ts
+++ src/common/guard/global/access-token.guard.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   type ExecutionContext,
   Injectable,
   UnauthorizedException,
@@ -12,12 +13,19 @@ import { BearerTokenGuard } from '@/common/guard/bearer-token.guard';
 @Injectable()
 export class AccessTokenGuard extends BearerTokenGuard {
   async canActivate(context: ExecutionContext) {
-    // 액세스 토큰이 필요 없는 요청
     const request = context.switchToHttp().getRequest();
     const path = request.route.path;
 
+    // 액세스 토큰이 필요 없는 요청
     if (path.startsWith('/auth/')) {
       return true;
+    }
+
+    if (
+      process.env.NODE_ENV === 'development' &&
+      !request.headers.authorization
+    ) {
+      return this.devAuthenticate(request);
     }
 
     // BearerTokenGuard 검증
@@ -33,6 +41,18 @@ export class AccessTokenGuard extends BearerTokenGuard {
     if (request.type !== 'access') {
       throw new UnauthorizedException('access 토큰이 아닙니다.');
     }
+
+    return true;
+  }
+
+  devAuthenticate(request: any) {
+    if (!request.headers.user_id) {
+      throw new BadRequestException(
+        'dev환경에서는 액세스토큰 미사용시 header에 user_id라는 key로 userId를 넣어주어야 합니다.',
+      );
+    }
+
+    request.userId = request.headers.user_id;
 
     return true;
   }
