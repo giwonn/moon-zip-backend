bf4dfe4390f3a0a7d20aadf58133309d7b92efab
Allen / 임기원 <l4279625@gmail.com>
2024-01-19 12:12:38 +0900

    userId 데코레이터 헤더에서 제대로 파싱 안되는 문제 수정 (#84)

--- src/common/decorator/user-id.decorator.ts
+++ src/common/decorator/user-id.decorator.ts
@@ -6,11 +6,11 @@ import {
 export const UserId = createParamDecorator((_, ctx) => {
   const request = ctx.switchToHttp().getRequest();
 
-  if (!request.userId) {
+  if (!request.headers['user-id']) {
     throw new InternalServerErrorException(
       'UserId 데코레이터를 사용하려면 AccessTokenGuard 또는 RefreshTokenGuard가 선행되어야 합니다.',
     );
   }
 
-  return request.userId;
+  return request.headers['user-id'];
 });
