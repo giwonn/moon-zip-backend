fcd65877001c1476179846f212f0260030fc3db1
Allen / 임기원 <l4279625@gmail.com>
2024-01-26 18:59:49 +0900

    libraryBook 추가 및 user 업데이트 및 리팩토링 (#103)

--- src/v1/library/dto/create-library.dto.ts
+++ src/v1/library/dto/create-library.dto.ts
@@ -1,22 +1,18 @@
 import { IsString } from 'class-validator';
 import { Builder } from 'builder-pattern';
-import { ApiProperty } from '@nestjs/swagger';
 import { Library } from '../entities/library.entity';
 
 export class CreateLibraryDto {
   @IsString()
-  bookId: string;
-  @IsString()
   userId: string;
 
   to(): Library {
-    return Builder<Library>().userId(this.userId).bookId(this.bookId).build();
+    return Builder<Library>().userId(this.userId).build();
   }
 
-  static of(userId: string, bookId: string) {
+  static of(userId: string) {
     const dto = new CreateLibraryDto();
     dto.userId = userId;
-    dto.bookId = bookId;
     return dto;
   }
 }
--- src/v1/library/entities/library.entity.ts
+++ src/v1/library/entities/library.entity.ts
@@ -1,8 +1,8 @@
 import type { Library as PrismaLibrary } from '@prisma/client';
 
 export class Library implements PrismaLibrary {
+  id: string;
   userId: string;
-  bookId: string;
   createdAt: Date;
   deletedAt: Date | null;
 }
